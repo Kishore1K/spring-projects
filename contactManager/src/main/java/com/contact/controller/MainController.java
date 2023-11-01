1c271dec28caee7b2ad5a4e72413411e6ac1749a
Kishore1K <reddykishore527@gmail.com>
2023-11-01 21:38:51 +0530

    UI

--- contactManager/src/main/java/com/contact/controller/MainController.java
+++ contactManager/src/main/java/com/contact/controller/MainController.java
@@ -5,6 +5,7 @@ import com.contact.entities.User;
 import com.contact.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -12,21 +13,22 @@ import java.util.List;
 @Controller
 public class MainController {
 
-    @Autowired
-    UserRepository userRepository;
-
-    @GetMapping("/test")
-    public  void example(){
-        User user = new User("Kishore", "reddyKishore@gmail1.com", "test", "admin", true, "/d/", "ABC");
-        Contact contact = new Contact("K", "K", "AB", "jdf", "123456789", "J", "kbd", user);
-        Contact contact1 = new Contact("K1", "K1", "AB1", "jdf1", "1231456789", "J1", "kbd1", user);
-        user.setContacts(List.of(contact1, contact));
-        userRepository.save(user);
-//
-//        System.out.println(user);
-//        System.out.println(contact);
-
+    @GetMapping("/")
+    public  String home(Model m){
+        m.addAttribute("title", "Home - Contact manager");
+        System.out.println("MainController.home");
+        return "home";
     }
+
+    @GetMapping("/about")
+    public  String about(Model m){
+        m.addAttribute("title", "About - Contact manager ");
+        System.out.println("MainController.home");
+        return "about";
+    }
+
+
+
 
 
 }
