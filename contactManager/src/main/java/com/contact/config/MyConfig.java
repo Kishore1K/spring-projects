b7bc23fcd64ca31f0d0b0cec66d3728cca4bb665
Kishore1K <reddykishore527@gmail.com>
2023-11-03 20:31:46 +0530

    Spring Security

--- contactManager/src/main/java/com/contact/config/MyConfig.java
+++ contactManager/src/main/java/com/contact/config/MyConfig.java
@@ -1,8 +1,35 @@
 package com.contact.config;
 
+import com.contact.service.UserDetailServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class MyConfig {
+@EnableWebSecurity
+public class MyConfig extends SecurityConfigurerAdapter {
+
+
+    @Bean
+    public UserDetailsService getUserDetailService(){
+        return  new UserDetailServiceImpl();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder  passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider=  new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(getUserDetailService());
+
+    }
 
 }
