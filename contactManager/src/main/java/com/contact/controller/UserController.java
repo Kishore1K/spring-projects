31b8fbf2f2aa09a4cf825cfa6a28bd6c2eeaabc6
Kishore1K <reddykishore527@gmail.com>
2023-11-05 19:12:40 +0530

    Add Contact Page

--- contactManager/src/main/java/com/contact/controller/UserController.java
+++ contactManager/src/main/java/com/contact/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.contact.controller;
 
+import com.contact.entities.Contact;
 import com.contact.entities.User;
 import com.contact.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -20,15 +18,28 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+
+    @ModelAttribute
+    public  void addCommon(Model m, Principal principal){
+        User user = userService.getUserName(principal.getName());
+        System.out.println(user.getName());
+        m.addAttribute("user", user);
+    }
     @GetMapping("/index")
     public  String dashboardHandler(Model m, Principal principal){
         System.out.println("UserController.dashboard");
         m.addAttribute("title", "DashBoard- Contact Manager");
         System.out.println(principal.getName());
-        User user = userService.getUserName(principal.getName());
-        System.out.println(user.getName());
-        m.addAttribute("user", user);
+
         return  "normal/user_dashboard";
+    }
+
+    @GetMapping("/add_contact")
+    public String openContactForm(Model m){
+        m.addAttribute("title", "Add Contact - Contact Manager");
+        m.addAttribute("contact", new Contact());
+        return "normal/add_contact";
+
     }
 
 
