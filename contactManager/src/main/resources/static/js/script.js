6b94e20546e9b2b5a57b238857454e056145e478
Kishore1K <reddykishore527@gmail.com>
2023-11-12 20:33:28 +0530

    Contact Manager Search

--- contactManager/src/main/resources/static/js/script.js
+++ contactManager/src/main/resources/static/js/script.js
@@ -7,3 +7,21 @@ const toggleSideBar =()=>{
         $(".content").css("margin-left", "20%");
     }
 };
+
+const  search =()=>{
+    let query = $("#search-input").val();
+    console.log(query);
+    if(query===''){
+        $(".search-result").hide();
+
+    }else{
+        let url = `http://localhost:8080/search/${query}`
+        fetch(url).then((res)=>{
+            return res.json();
+        })
+            .then((data)=>{
+                console.log(data)
+            })
+        $(".search-result").show();
+    }
+}
