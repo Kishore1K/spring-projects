54cacdab0bd2f9ae363d691f43251d368a04d7a3
Kishore1K <reddykishore527@gmail.com>
2023-11-13 13:16:09 +0530

    Tasks

--- UserTask/src/main/java/com/tasks/controller/TaskController.java
+++ UserTask/src/main/java/com/tasks/controller/TaskController.java
@@ -1,11 +1,27 @@
 package com.tasks.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tasks.model.TaskDTO;
+import com.tasks.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+    @PostMapping("/{id}/tasks")
+    public ResponseEntity<TaskDTO> saveTask(
+            @RequestParam("id") Long userId, @RequestBody TaskDTO taskDTO
+    ){
+
+        return  new ResponseEntity<>(taskService.saveTask(userId, taskDTO), HttpStatus.CREATED);
+
+    }
+
 
 
 }
--- UserTask/src/main/java/com/tasks/service/TaskServiceImpl.java
+++ UserTask/src/main/java/com/tasks/service/TaskServiceImpl.java
@@ -1,12 +1,19 @@
 package com.tasks.service;
 
 import com.tasks.model.TaskDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TaskServiceImpl implements TaskService{
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
     @Override
     public TaskDTO saveTask(Long userId, TaskDTO taskDTO) {
         return null;
