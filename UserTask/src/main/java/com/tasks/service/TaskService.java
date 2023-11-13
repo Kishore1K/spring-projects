af8dafa2c6b74357879eb7266b1a9e46175bd739
Kishore1K <reddykishore527@gmail.com>
2023-11-13 13:08:17 +0530

    Tasks

--- UserTask/src/main/java/com/tasks/service/TaskService.java
+++ UserTask/src/main/java/com/tasks/service/TaskService.java
@@ -1,4 +1,13 @@
 package com.tasks.service;
 
+import com.tasks.model.TaskDTO;
+
+import java.util.List;
+
 public interface TaskService {
+
+    public TaskDTO saveTask(Long userId, TaskDTO taskDTO);
+
+    public List<TaskDTO> getAllTasks(Long userId);
+
 }
--- UserTask/src/main/java/com/tasks/service/TaskServiceImpl.java
+++ UserTask/src/main/java/com/tasks/service/TaskServiceImpl.java
@@ -1,7 +1,19 @@
 package com.tasks.service;
 
+import com.tasks.model.TaskDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskServiceImpl implements TaskService{
+    @Override
+    public TaskDTO saveTask(Long userId, TaskDTO taskDTO) {
+        return null;
+    }
+
+    @Override
+    public List<TaskDTO> getAllTasks(Long userId) {
+        return null;
+    }
 }
