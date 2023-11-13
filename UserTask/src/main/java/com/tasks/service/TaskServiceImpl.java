b6c9f2b6eea2a1a6678d8cc1c0dc1c4525335174
Kishore1K <reddykishore527@gmail.com>
2023-11-13 16:07:56 +0530

    Tasks

--- UserTask/src/main/java/com/tasks/service/TaskServiceImpl.java
+++ UserTask/src/main/java/com/tasks/service/TaskServiceImpl.java
@@ -1,6 +1,10 @@
 package com.tasks.service;
 
+import com.tasks.entity.Task;
+import com.tasks.entity.User;
 import com.tasks.model.TaskDTO;
+import com.tasks.repository.TasksRepository;
+import com.tasks.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,10 +17,17 @@ public class TaskServiceImpl implements TaskService{
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private UserRepository userRepository;
 
+    @Autowired
+    private TasksRepository tasksRepository;
     @Override
     public TaskDTO saveTask(Long userId, TaskDTO taskDTO) {
-        return null;
+        User user=userRepository.findById(userId).get();
+        Task  task = modelMapper.map(taskDTO, Task.class);
+        Task savedTask  = tasksRepository.save(task);
+        return modelMapper.map(savedTask, TaskDTO.class);
     }
 
     @Override
