de8a5172e4613189f0d2a2f9f66522471514f345
Kishore1K <reddykishore527@gmail.com>
2023-11-24 19:05:11 +0530

    Exam Sys DB

---
+++ exam/src/main/java/com/exam/entity/Questions.java
@@ -0,0 +1,30 @@
+package com.exam.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Questions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String question;
+
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private int op_ans;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Exams> exams;
+
+}
